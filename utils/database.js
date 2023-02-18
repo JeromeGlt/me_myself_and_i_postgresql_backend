b17e79963c14d69cc61bfe9e3c79a6c702440dd1
JeromeGlt <jerome.gallot39@orange.fr>
2023-02-18 15:43:55 +0100

    connexion with an admin user at the database

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_LOGIN, process.env.DB_PASSWORD, {
     host: 'localhost',
-    dialect: 'pg'
+    dialect: 'postgres'
 })
 
 module.exports = sequelize
